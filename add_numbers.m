516cdc519a1a184a8a705f630dba8af8c0afbeb8
Huda Shakir <68558969+Huda-sh@users.noreply.github.com>
2024-03-01 15:17:02 +0300

    Update add_numbers.m

--- add_numbers.m
+++ add_numbers.m
@@ -2,3 +2,12 @@ function c = add_numbers(a, b)
     c = a + b;
 end
 
+%!test
+%! assert(add_numbers(1, 2), 3);
+%! assert(add_numbers(-1, 2), 1);
+%! assert(add_numbers(0, 0), 0);
+
+%!test
+%! assert(add_numbers(1, 2), 3);
+%! assert(add_numbers(-1, 2), 1);
+%! assert(add_numbers(0, 0), 0);
